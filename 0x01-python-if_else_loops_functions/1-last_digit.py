92692eaae50b83ee6f18cedd8d657cb858cea1b6
josh.94 <jeshua.cabanillas@gmail.com>
2021-12-21 15:06:58 -0500

    First commit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    renum = number % -10
+else:
+    renum = number % 10
+print("The last digit of {:d} is {:d}".format(number, renum), end=" ")
+if((renum) > 5):
+    print("and is greater than 5")
+elif((renum) == 0):
+    print("and is 0")
+elif((renum) < 6 and not 0):
+    print("and is less than 6 and not 0")
