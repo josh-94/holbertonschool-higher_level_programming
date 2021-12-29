e34e1f70a8efac8f05a90a9bf9bb83f469b5ce27
josh.94 <jeshua.cabanillas@gmail.com>
2021-12-29 11:04:25 -0500

    fix: line 8, rest counting number arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if len(sys.argv) == 1:
     print("0 arguments.")
 elif len(sys.argv) == 2:
     print("{} argument:".format(len(sys.argv) - 1))
-    print("{}: {}".format(len(sys.argv), sys.argv[1]))
+    print("{}: {}".format(len(sys.argv) - 1, sys.argv[1]))
 else:
     print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
