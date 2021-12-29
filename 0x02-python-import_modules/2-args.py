ab45d54e1e3bc3e55347c5febe5611e68c47abe6
josh.94 <jeshua.cabanillas@gmail.com>
2021-12-29 10:47:40 -0500

    First commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) == 1:
+    print("0 arguments.")
+elif len(sys.argv) == 2:
+    print("{} argument:".format(len(sys.argv) - 1))
+    print("{}: {}".format(len(sys.argv), sys.argv[1]))
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
