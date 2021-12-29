5e14ebcf19bc2b8f8a9ce14d263fd1df72b5a5f9
josh.94 <jeshua.cabanillas@gmail.com>
2021-12-29 14:53:05 -0500

    refactor: chaging code

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range(1, 90):
-    if(i == 89):
-        print("{:02d}".format(i))
-        break
-    if (i == 10 or i == 11):
-        continue
-    print("{:02d}".format(i), end=", ")
+for i in range(0, 9):
+    for j in range(1, 10):
+        if (i == 8 and j == 9):
+            print("{:d}{:d}".format(i, j))
+            break
+        if(i < j):
+            print("{:d}{:d}, ".format(i, j), end="")
