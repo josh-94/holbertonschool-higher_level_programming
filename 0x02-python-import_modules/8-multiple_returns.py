b0ef038841504bdf6a54f533546aa95d79dde7a8
josh.94 <jeshua.cabanillas@gmail.com>
2022-01-04 14:51:43 -0500

    First commit

---
+++ 0x02-python-import_modules/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence is "":
+        first_chr is None
+    else:
+        first_chr = sentence[0]
+        return(len(sentence), first_chr)
