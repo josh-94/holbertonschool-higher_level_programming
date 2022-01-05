f590b52588fb97809c466e4e7f70b4083820ce91
josh.94 <jeshua.cabanillas@gmail.com>
2022-01-05 13:36:27 -0500

    style: missing whitespace line 5

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
-        new_matrix.append(list(map(lambda i:i*i, row)))
+        new_matrix.append(list(map(lambda i: i*i, row)))
     return new_matrix
