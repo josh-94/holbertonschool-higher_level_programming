877ed502e8c5a139f03b9bcbec87c3f4f80f05a3
josh.94 <jeshua.cabanillas@gmail.com>
2022-01-31 18:46:53 -0500

    style: pycodestyle

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,7 +3,7 @@
 
 
 class MyList(list):
-        """Class MyList inherited"""
-        def print_sorted(self):
-            """prints sorted list"""
-            print(sorted(self))
+    """Class MyList inherited"""
+    def print_sorted(self):
+        """prints sorted list"""
+        print(sorted(self))
