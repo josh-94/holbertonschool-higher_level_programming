8b738ad9233c49d7a9ccfdad504c1888ae531190
josh.94 <jeshua.cabanillas@gmail.com>
2022-02-11 15:12:59 -0500

    updating class to_jason

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
-''' first class Base '''
+"""Class"""
+
+
+import json
+from queue import Empty
 
 
 class Base:
-    ''' class Base'''
+    """Base class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialize method"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of
+        list_dictionaries """
+        if list_dictionaries is None or Empty:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
