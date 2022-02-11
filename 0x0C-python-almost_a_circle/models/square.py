4b6e1037ee8bbeb5bcafe58f284cfba70d5cdccc
josh.94 <jeshua.cabanillas@gmail.com>
2022-02-11 13:09:51 -0500

    First commit square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+First Square
+Class Square that inherits from Rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialization of data"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """retrieves the size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Validation must inherit from Rectangle"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        square = "[Square] ({}) {}/{} - {}"
+        square = square.format(self.id, self.x, self.y, self.width)
+        return square
