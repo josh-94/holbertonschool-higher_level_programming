b9399a155335cc399651024b47464f8f5b70bc85
josh.94 <jeshua.cabanillas@gmail.com>
2022-04-07 21:49:46 -0500

    Add: task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const num = parseInt(process.argv[2]);
+function factorial (num) {
+  if (isNaN(num) || num === 1) {
+    return (1);
+  } else {
+    return (num * factorial(num - 1));
+  }
+}
