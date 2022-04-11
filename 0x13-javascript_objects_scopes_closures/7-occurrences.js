2a9123cb7574cfd699a56f88cd0eb9fb4b2c14ae
josh.94 <jeshua.cabanillas@gmail.com>
2022-04-11 16:19:10 -0500

    style: semistandard

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let suma = 0;
-    for (let i of list) {
-        if (i == searchElement) suma += 1;
-    } return (suma);
-}
+  let suma = 0;
+  for (const i of list) {
+    if (i === searchElement) suma += 1;
+  } return (suma);
+};
