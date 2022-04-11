a1cfaf7bb1818276d8425f0d86601ce27ecf7364
josh.94 <jeshua.cabanillas@gmail.com>
2022-04-11 12:51:55 -0500

    test: checker

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -7,21 +7,23 @@ class Rectangle {
     }
   }
 
-  print (c = 'X') {
+  print () {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        process.stdout.write(c);
+        process.stdout.write('X');
       }
       process.stdout.write('\n');
     }
   }
 
   rotate () {
-    [this.width, this.height] = [this.height, this.width];
+    [this.width = this.height];
+    [this.height = this.width];
   }
 
   double () {
-    [this.width, this.height] = [this.width * 2, this.height * 2];
+    [this.width = this.width * 2];
+    [this.height = this.height * 2];
   }
 }
 module.exports = Rectangle;
