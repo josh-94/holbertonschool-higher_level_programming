6c6414e52c02c39c22ef22fb4346dcda6fb2de0b
josh.94 <jeshua.cabanillas@gmail.com>
2022-04-13 12:47:45 -0500

    Fix: line 13 working on case-sensitive BINARY

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
         host='localhost', port=3306, user=sys.argv[1],
         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT id, name FROM states WHERE name LIKE'N%'
+    cur.execute("""SELECT id, name FROM states WHERE name LIKE BINARY'N%'
                 ORDER BY id ASC""")
     rows = cur.fetchall()
     for row in rows:
