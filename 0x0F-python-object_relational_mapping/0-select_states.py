ed0907f1283983dbffef8eb60129fd307013c6e3
josh.94 <jeshua.cabanillas@gmail.com>
2022-04-13 12:18:00 -0500

    fix: ascending order by id

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa"""
 if __name__ == '__main__':
     import MySQLdb
     import sys
@@ -7,7 +8,7 @@ if __name__ == '__main__':
         host='localhost', port=3306, user=sys.argv[1],
         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states ASC")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
