3e2e1eeed61d69d04565307fa58e1c295dc5a837
josh.94 <jeshua.cabanillas@gmail.com>
2022-04-22 16:40:18 -0500

    fix: decode

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
 
-    print("Body response:")
-    print("\t- type:", type(html))
-    print("\t- content:", (html))
-    print("\t- utf8 content: " + response.msg)
+        print("Body response:")
+        print("\t- type:", type(html))
+        print("\t- content:", html)
+        print("\t- utf8 content: " + html.decode('utf-8'))
