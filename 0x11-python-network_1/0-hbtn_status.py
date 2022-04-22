a2bc0a192ae8c462d0a7b184b12fc7f5cb1eeff1
josh.94 <jeshua.cabanillas@gmail.com>
2022-04-22 16:33:16 -0500

    refactor: code should not be imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import urllib.request
-'''Python script that fetches https://intranet.hbtn.io/status'''
+# '''Python script that fetches https://intranet.hbtn.io/status'''
+if __name__ == "__main__":
+    import urllib.request
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
 
     print("Body response:")
-    print("\t- type: ", type(html))
+    print("\t- type:", type(html))
     print("\t- content: " + str(html))
     print("\t- utf8 content: " + response.msg)
