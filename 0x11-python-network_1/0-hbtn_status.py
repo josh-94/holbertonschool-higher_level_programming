639209f42b4b5badfbbf541348b47b2270728fa8
josh.94 <jeshua.cabanillas@gmail.com>
2022-04-22 16:35:17 -0500

    style: pycode style

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-# '''Python script that fetches https://intranet.hbtn.io/status'''
+'''Python script that fetches https://intranet.hbtn.io/status'''
 if __name__ == "__main__":
     import urllib.request
-
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
 
     print("Body response:")
     print("\t- type:", type(html))
