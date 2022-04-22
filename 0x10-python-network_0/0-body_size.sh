22e9c1932ba86d3f7ae9cb8dcbcf6623668ca46b
josh.94 <jeshua.cabanillas@gmail.com>
2022-04-21 23:05:17 -0500

    test: task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script takes URL, sends a request and displays the size of the body of th response.
-curl -I -s "$1" | grep "Content-Length" | cut -d ' ' -f 2
+curl -s -I ${1} | grep "Content-Length" | cut -d ' ' -f 2
