3726d00539392a155f18220eac0e66da7986905c
josh.94 <jeshua.cabanillas@gmail.com>
2022-05-25 21:37:15 -0500

    update: new code improved

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,2 +1,7 @@
-$.ajax('https://swapi-api.hbtn.io/api/people/5/?format=json')
-  .done((data) => $('#character').append(data.name));
+$.ajax('https://swapi-api.hbtn.io/api/people/5/?format=json',
+  {
+    success: function (data) {
+      $('#character').append(data.name);
+    }
+
+  });
